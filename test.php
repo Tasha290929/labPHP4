330e0ed6cddeac329feeaa1d2dd1d5e0189007e5
Tasha290929 <138973005+Tasha290929@users.noreply.github.com>
2024-03-08 23:16:57 +0200

    first commit

--- test.php
+++ test.php
@@ -1,61 +1,82 @@
-<h3>Ваше имя?</h3>
-<form action='server.php' method='POST'>
-   <input name='name' class = "<?php echo $class_name?>" placeholder='Введите имя' value="<?php if(isset($_POST['name'])) echo $_POST['name2'] ?>">
-   <input type='hidden' name='checkbox' value='0'>
-   <input type='checkbox' name='checkbox' value='1'> 
-   <button type='submit' value='1'>Click Me!</button>
-</form>
-<h3>бывали ли Вы в Италии?</h3>
-<form action='server.php' method='POST'>
-   <input type='radio' name='lang' value='Да' id ='yes' checked> <!-- отмеченная кнопка -->
-   <label for='yes'>Да</label>
-   <input type='radio' name='lang' value='Нет' id ='no'> 
-   <label for='no'>Нет</label>
-   <button type='submit' value='1'>Click Me!</button>
-</form>
-<h3>Ваше образование?</h3>
-<form action='server.php' method='POST'>
-   <input type='radio' name='education' value='среднее' id ='average'> 
-   <label for='no'>среднее</label>
-   <input type='radio' name='education' value='высшее' id ='higher'> 
-   <label for='no'>высшее</label>
-   <input type='radio' name='education' value='незаконченное высшее' id ='incomplete'> 
-   <label for='no'>незаконченное высшее</label>
-   <button type='submit' value='1'>Click Me!</button>
-</form>
-<h3>Ваш пол?</h3>
-<form action='server.php' method='POST'>
-	<select name='sex'>
-		<option value='1'>Муж</option>
-		<option value='2'>Жен</option>
-	</select>
-	<button type='submit' value='1'>Click Me!</button>
-</form>
-<?php //SERVER.php--в файле сервер
- /*Спросите у пользователя имя с помощью формы. Сделайте чекбокс: если он отмечен, то поприветствуйте пользователя, если не отмечен - попрощайтесь с пользователем. 
-Спросите у пользователя бывал ли он в Италии c помощью двух radio-кнопок. Выведите результат на экран. Сделайте так, чтобы по умолчанию один из вариантов был уже отмечен.
-Спросите у пользователя его образование с помощью нескольких radio-кнопок. Варианты ответа сделайте такими: среднее, высшее, незаконченное высшее
-  
-Спросите у пользователя его образование с помощью выпадающего списка. Варианты ответа сделайте такими: среднее, высшее, незаконченное высшее
-  
-Спросите у пользователя его пол используя выпадающий список.
-*/
-if($_POST['checkbox']=='1') {
-      echo 'Привет,  '.$_POST['name'].'<br>';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    // объявляем переменные и устанавливаем пустые значения
+    $nameErr = "";
+    $name ="";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+            $nameErr = "Введите имя";
+        } else {
+            $name = test_input($_POST["name"]);
+            // проверяем, содержит ли имя только буквы и пробелы
+            if (!preg_match("/^(([a-zA-Z' -]{3,20})|([а-яА-ЯЁёІіЇїҐґЄє' -]{3,20}))$/u", $name)) {
+                $nameErr = "Имя должно содержать только буквы и пробелы";
+            }
+        }
     }
-if($_POST['checkbox']=='0'){
-  echo 'Пока,  '.$_POST['name'].'<br>';
-  }
+    
+    ?>
 
-$lang = $_POST['lang'];
-echo $lang.'<br>';
+<div class="block">
+<h1> Пройти тест </h1>
 
-$education = $_POST['education'];
-echo $education.'<br>';
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <label name="name"> Name:
+                <input type="text" name="name" value="<?php echo $name; ?>">
+                <span class="error">* <?php echo $nameErr; ?></span>
+                <br> <br>
+            </label>
 
-$sex = $_POST['sex'];
-echo $sex.'<br>';
-?>
-<form action='Php5Tasks.php' method='POST' enctype="multipart/form-data">
-  <input type="submit" value="Главная">
+ <h3>Какое животное не умеет чихать ?</h3>
+            <div class="group">
+	<input type="radio" name="test" value="1"> - Собака<br>
+	<input type="radio" name="test" value="2"> - Слон<br>
+	<input type="radio" name="test" value="3"> - Кролик<br>
+    <input type="radio" name="test" value="4"> - Кошка<br>
+
+</div>
+
+<h3>Какое животное видит мир в черно - белых цветах ?</h3>
+
+<div class="group">
+	<input type="radio" name="test" value="1"> - Собака<br>
+	<input type="radio" name="test" value="2"> - Сова<br>
+	<input type="radio" name="test" value="3"> - Гепард<br>
+    <input type="radio" name="test" value="4"> - Змея<br>//
+</div>
+
+<h3>Какому народу приписывается изобретение шахмат ?</h3>
+
+<div class="group">
+	<input type="radio" name="test" value="1"> - Китайцы<br>
+	<input type="radio" name="test" value="2"> - Египтяне<br>
+	<input type="radio" name="test" value="3"> - Индийцы<br>//
+    <input type="radio" name="test" value="4"> - Греки<br>
+</div>
+
+
+<h3>Какой алкогольный напиток самый популярный в мире ?</h3>
+
+<div class="group">
+	<input type="radio" name="test" value="1"> - Водка<br>
+	<input type="radio" name="test" value="2"> - Виски<br>//
+	<input type="radio" name="test" value="3"> - Вино<br>
+    <input type="radio" name="test" value="4"> - Пиво<br>
+</div>
+
 </form>
+
+
+</div>    
+
+</body>
+</html>
